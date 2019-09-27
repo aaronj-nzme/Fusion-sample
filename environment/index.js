ad218c8534bee8b9636c4a58705c5e442b4b9892
NanJiang327 <nj499521010@gmail.com>
2019-09-27 22:54:36 +1200

    Using Environment Variables and Secrets

---
+++ environment/index.js
@@ -0,0 +1,6 @@
+/*  /environment/index.js  */
+// To use the tool, simply go to your environment's Maestro "Secrets" page. 
+// Copy the "secret" value for our OMDB_API_KEY and paste it into the "encrypt" text input at the top of the page, then click the "encrypt" button. You should get an encrypted value that looks similar to this:
+export default {
+  OMDB_API_KEY: "%{AQECAHhPwAyPK3nfERyAvmyWOWx9c41uht+ei4Zlv4NgrlmypwAAAMYwgcMGCSqGSIb3DQEHBqCBtTCBsgIBADCBrAYJKoZIhvcNAQcBMB4GCWCGSAFlAwQBLjARBAxwBJdfzqcQUpox1xsCARCAf2aXwBJ3pBUP12HWB3cdBboV1/qN0HFEsjNycADYIq7XSANeDYOlu2/Dwt/52R16hK4dbVOt0ofNKKx0b3vtZRaH9bX1Dkx6TDhmo5g32H0aWpiUW6PQIp72/g2CW1nr26T0zxmkxmX9u8ufoQGBXRd1pOfT2EliUhMKabNeSyk=}"
+}
